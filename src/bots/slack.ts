490cebd303a936dbe105b445c77993ad697b37aa
Gen Ichino <zenoplex@gmail.com>
2017-06-01 01:12:15 +0900

    feat: add ping listener

--- src/bots/slack.ts
+++ src/bots/slack.ts
@@ -1,4 +1,11 @@
 import * as Botkit from 'botkit';
+import * as env from '../env';
 
 const controller = Botkit.slackbot({});
-controller.spawn({ token: 'test' }).startRTM();
+controller.spawn({ token: env.slackBotToken }).startRTM((err) => {
+  if (err) throw new Error('Could not connect to Slack');
+});
+
+controller.hears('ping', ['direct_message', 'direct_mention', 'mention', 'ambient'], (bot, message) => {
+  console.log(bot, message);
+});
