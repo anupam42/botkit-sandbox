21feb3deacee7d4d26ffea882ee3d45b117aed0e
Gen Ichino <zenoplex@gmail.com>
2017-06-02 01:58:51 +0900

    fix: remove amibient

--- src/controls/slack/static.ts
+++ src/controls/slack/static.ts
@@ -2,10 +2,9 @@ import { Controller } from 'botkit';
 import * as R from 'ramda';
 
 export const ping
-: (_: Controller) => Controller
-= R.tap<Controller>(controller => controller.hears(
-  /^ping$/, 
-  ['direct_message', 'direct_mention', 'mention', 'ambient'],
-  (bot, message) => bot.reply(message, 'pong'),
-  ),
-);
+  : (_: Controller) => Controller
+  = R.tap<Controller>(controller => controller.hears(
+    /^ping$/,
+    ['direct_message', 'direct_mention', 'mention'],
+    (bot, message) => bot.reply(message, 'pong'),
+  ));
