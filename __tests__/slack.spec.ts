0154cb55dca6a6a397ae327588506e5a6c99d6fa
Gen Ichino <zenoplex@gmail.com>
2017-06-05 15:58:22 +0900

    test: add more ping test

--- __tests__/slack.spec.ts
+++ __tests__/slack.spec.ts
@@ -1,33 +1,65 @@
-import assert from 'power-assert';
-import * as Botmock from 'botkit-mock';
+import * as assert from 'power-assert';
+import Botmock = require('botkit-mock');
 import * as controllers from '../src/controllers/slack';
 
 describe('slack bot', () => {
+  const baseMessage = {
+    user: 'feedforce',
+    channel: 'randomChannel',
+  };
   /* tslint:disable: no-let */
-  let mockController: any;
-  let bot;
+  let mockController: BotkitMock.Controller;
+  let bot: BotkitMock.Bot;
   /* tslint:enable: no-let */
 
   beforeEach(() => {
     mockController = Botmock({ stats_optout: true, debug: false });
     bot = mockController.spawn({ type: 'slack' });
-    controllers.ping(mockController);
   });
 
   afterEach(() => {
     mockController.shutdown();
   });
 
-  it('should return `pong` if user types `ping`', async (done) => {
-    const message = await bot.usersInput([{
-      user: 'feedforce',
-      channel: 'dfplusio-devel',
-      messages: [
-        { text: 'ping', isAssertion: true },
-      ],
-    }]);
+  describe('ping', () => {
+    beforeEach(() => controllers.ping(mockController as any));
 
-    assert(message.text === 'pong');
-    done();
+    it('should return `pong` if user types `ping`', async (done) => {
+      const message = await bot.usersInput([{
+        ...baseMessage,
+        messages: [
+          { text: 'ping', isAssertion: true },
+        ],
+      }]);
+
+      assert(message.text === 'pong');
+      done();
+    });
+
+    it('should return `pong` if user type `Ping`', async (done) => {
+      const message = await bot.usersInput([{
+        ...baseMessage,
+        messages: [
+          { text: 'Ping', isAssertion: true },
+        ],
+      }]);
+
+      assert(message.text === 'pong');
+      done();
+    });
+
+    it('should not return ping if user ping is not a word', async (done) => {
+      const message = await bot.usersInput([
+        {
+          ...baseMessage,
+          messages: [
+            { text: 'pingu', isAssertion: true },
+          ],
+        },
+      ]);
+
+      assert(message.text === undefined);
+      done();
+    });
   });
 });
