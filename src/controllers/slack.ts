146931aeb8c02b66fa1920155ebacb12c2ad0439
Gen Ichino <zenoplex@gmail.com>
2017-06-05 15:58:15 +0900

    refactor: allow ping command to be case insensitive

--- src/controllers/slack.ts
+++ src/controllers/slack.ts
@@ -4,7 +4,7 @@ import * as R from 'ramda';
 export const ping
   : (_: Controller) => Controller
   = R.tap<Controller>(controller => controller.hears(
-    /^ping$/,
+    /^ping$/i,
     ['direct_message', 'direct_mention', 'mention'],
     (bot, message) => bot.reply(message, 'pong'),
   ));
